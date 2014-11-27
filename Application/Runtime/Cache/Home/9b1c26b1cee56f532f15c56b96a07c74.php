c7323e4b67f6b383cbcd38457456affcce55f218
peifei <peifeiml@126.com>
2014-11-27 10:15:56 +0800

    从新调整了目录结构，将index.php及.htaccess文件移动到public文件夹下，添加了Backbone模块

---
+++ Application/Runtime/Cache/Home/9b1c26b1cee56f532f15c56b96a07c74.php
@@ -0,0 +1,11 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+THIS IS LAYOUT TEXT
+<body>
+<P>this is content text</P>
+</body>
+</html>
