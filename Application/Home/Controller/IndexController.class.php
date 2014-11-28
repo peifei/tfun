c15f4018e2f4619e792748da40cabc69cbf484f6
peifei <peifeiml@126.com>
2014-11-28 14:11:18 +0800

    20141128

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,6 +3,10 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
+        $this->title='peifei';
+        $fun = M('Fun');
+        $this->list=$fun->order('id desc')->select();
+        //trace($fun->order('id desc')->select());
         $this->display();
     }
 }
--- Application/Runtime/Cache/Home/9b1c26b1cee56f532f15c56b96a07c74.php
+++ Application/Runtime/Cache/Home/9b1c26b1cee56f532f15c56b96a07c74.php
@@ -2,10 +2,30 @@
 <html>
 <head lang="en">
     <meta charset="UTF-8">
-    <title></title>
+    <title><?php echo ($title); ?></title>
+    <link type="text/css" rel="stylesheet" href="/tfun/public/css/bootstrap.css"/>
+    <link type="text/css" rel="stylesheet" href="/tfun/public/css/mystyle.css"/>
 </head>
-THIS IS LAYOUT TEXT
 <body>
-<P>this is content text</P>
+<nav class="navbar navbar-default" role="navigation">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">TFUN</a>
+        </div>
+</nav>
+
+<?php if(is_array($list)): $i = 0; $__LIST__ = $list;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$record): $mod = ($i % 2 );++$i;?><div class="col-lg-3 col-md-4 col-sm-6">
+        <div class="panel panel-default">
+            <?php if(!empty($record['title'])){ ?>
+                <div class="panel-heading">
+                    <h3 class="panel-title"><?php echo ($record['title']); ?></h3>
+                </div>
+            <?php }?>
+            <div class="panel-body">
+                Basic panel example
+            </div>
+        </div>
+    </div><?php endforeach; endif; else: echo "" ;endif; ?>
+<script type="javascript" src="/tfun/public/js/jquery-1.10.2.min.js"></script>
+<script type="javascript" src="/tfun/public/js/bootstrap.js"></script>
 </body>
 </html>
