bdf75edf582565b81f5c89975e36c41127f60a99
peifei <peifeiml@126.com>
2014-11-28 15:28:57 +0800

    设置了动态布局及展示模块

--- Application/Runtime/Cache/Home/9b1c26b1cee56f532f15c56b96a07c74.php
+++ Application/Runtime/Cache/Home/9b1c26b1cee56f532f15c56b96a07c74.php
@@ -12,16 +12,26 @@
             <a class="navbar-brand" href="#">TFUN</a>
         </div>
 </nav>
-
 <?php if(is_array($list)): $i = 0; $__LIST__ = $list;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$record): $mod = ($i % 2 );++$i;?><div class="col-lg-3 col-md-4 col-sm-6">
         <div class="panel panel-default">
-            <?php if(!empty($record['title'])){ ?>
-                <div class="panel-heading">
-                    <h3 class="panel-title"><?php echo ($record['title']); ?></h3>
+            <div class="panel-heading">
+                <?php if(empty($$record['title'])): ?><h3 class="panel-title">没标题</h3>
+                <?php else: ?>
+                    <h3 class="panel-title"><?php echo ($record['title']); ?></h3><?php endif; ?>
+            </div>
+            <div class="panel-body tfun-content">
+                <div class="content-conatiner">
+                    <?php echo ($record['content']); ?>
                 </div>
-            <?php }?>
-            <div class="panel-body">
-                Basic panel example
+            </div>
+            <div class="panel-footer">
+                <div class="dz">
+                    <span class="glyphicon glyphicon-thumbs-up"></span>赞
+                </div>
+
+                <div class="zk">
+                    <span class="glyphicon glyphicon-plus-sign"></span>展开全部
+                </div>
             </div>
         </div>
     </div><?php endforeach; endif; else: echo "" ;endif; ?>
